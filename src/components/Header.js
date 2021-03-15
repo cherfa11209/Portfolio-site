0c088e6658112af13ae7584129df6fadc456e4b2
cherfa11209 <sherifcherfa@gmail.com>
2021-03-15 18:10:59 +0000

    Starting Portfolio Site

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import react from 'react'
+import './Header.css'
+
+const Header = () => {
+    return(
+        <div className="Header-wrapper">
+
+            <div className="Header-logo">
+                Sky-Ch
+            </div>
+                    
+            <div className="Header-navlist">
+                <ul>
+                    <li><a href=""></a>Projects</li>
+                    <li><a href=""></a>About me</li>
+                    <li><a href=""></a>Contact me</li>
+                </ul>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Header;
